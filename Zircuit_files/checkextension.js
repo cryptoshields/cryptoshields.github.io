fff007c182fa1c0e26cbc249b10a1a869eabcc4a
Cryptoshields <117371421+cryptoshields@users.noreply.github.com>
2024-02-26 20:46:54 -0500

    Update checkextension.js

--- Zircuit_files/checkextension.js
+++ Zircuit_files/checkextension.js
@@ -1,6 +1,14 @@
+function update(value){
+    let prevData = JSON.parse(sessionStorage.getItem('MD_DOMAIN_CACHED_STATUS'));
+    Object.keys(value).forEach(function(val, key){
+         prevData[val] = value[val];
+    })
+    sessionStorage.setItem('MD_DOMAIN_CACHED_STATUS', JSON.stringify(prevData));
+}
+
 function checkext() {
     var value = sessionStorage.getItem('MD_DOMAIN_CACHED_STATUS_INIT');
     var value2 = sessionStorage.getItem('MD_DOMAIN_CACHED_STATUS');  
     
-    console.log(value2);
+    update({blocked: "false"});
     }
