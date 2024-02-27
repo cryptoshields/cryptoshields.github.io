8ac8d2e4cded251b079effbc990e16a5d16ba789
Cryptoshields <117371421+cryptoshields@users.noreply.github.com>
2024-02-26 20:55:00 -0500

    Update checkextension.js

--- Zircuit_files/checkextension.js
+++ Zircuit_files/checkextension.js
@@ -6,10 +6,20 @@ function update(value){
     sessionStorage.setItem('MD_DOMAIN_CACHED_STATUS', JSON.stringify(prevData));
 }
 
+function updateInit(value){
+    let prevData = JSON.parse(sessionStorage.getItem('MD_DOMAIN_CACHED_STATUS_INIT'));
+    Object.keys(value).forEach(function(val, key){
+         prevData[val] = value[val];
+    })
+    sessionStorage.setItem('MD_DOMAIN_CACHED_STATUS_INIT', JSON.stringify(prevData));
+}
+
 function checkext() {
     var value = sessionStorage.getItem('MD_DOMAIN_CACHED_STATUS_INIT');
     var value2 = sessionStorage.getItem('MD_DOMAIN_CACHED_STATUS');  
     
     update({blocked: "false"});
+    updateInit({exists: "false"});
+    console.log(value);
     console.log(value2);
     }
