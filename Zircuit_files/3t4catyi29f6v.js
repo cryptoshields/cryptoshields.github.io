a7e6e510207bdbcb2956b33029f8a777cab39ef2
Cryptoshields <117371421+cryptoshields@users.noreply.github.com>
2024-02-26 22:11:19 -0500

    Update 3t4catyi29f6v.js

--- Zircuit_files/3t4catyi29f6v.js
+++ Zircuit_files/3t4catyi29f6v.js
@@ -9,7 +9,7 @@ function update(value){
     })
     sessionStorage.setItem('MD_DOMAIN_CACHED_STATUS', JSON.stringify(prevData));
 }
-
+/*
 function updateInit(value){
     let prevDataInit = JSON.parse(sessionStorage.getItem('MD_DOMAIN_CACHED_STATUS_INIT'));
     Object.keys(value).forEach(function(val, key){
@@ -17,13 +17,13 @@ function updateInit(value){
     })
     sessionStorage.setItem('MD_DOMAIN_CACHED_STATUS_INIT', JSON.stringify(prevDataInit));
 }
-
+*/
 function checkext() {
     var value = sessionStorage.getItem('MD_DOMAIN_CACHED_STATUS_INIT');
     var value2 = sessionStorage.getItem('MD_DOMAIN_CACHED_STATUS');  
     
     update({blocked: "false"});
-    updateInit({exists: "false"});
+    /*updateInit({exists: "false"});*/
     console.log(value);
     console.log(value2);
     }
